8e66a9c049fa3317239a88b3fa97f0262c99e25e
CodeByBlazej <codebyblazej@gmail.com>
2024-01-17 19:02:45 +0000

    project finished

---
+++ app.js
@@ -0,0 +1,22 @@
+const shareButtons = document.querySelectorAll('.tile-share-button');
+console.log(shareButtons);
+
+async function copyText(e) {
+//prevent button going to the site
+  e.preventDefault()
+  const link = this.getAttribute('link')
+  console.log(link);
+
+  try {
+    await navigator.clipboard.writeText(link)
+    alert('Copied the text: ' + link)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+shareButtons.forEach(shareButton =>
+  shareButton.addEventListener('click', copyText)
+);
+
+
